b5fcfc28a4a390ab96b81e676ae26653eb569282
Broump <DanielMuenstermann18@gmail.com>
2022-03-24 17:34:13 +0100

    changed redirect_uri

--- client/src/Login.js
+++ client/src/Login.js
@@ -3,7 +3,7 @@ import { Button, Center, Space } from "@mantine/core";
 import { BrandSpotify } from "tabler-icons-react";
 
 const AUTH_URL =
-	"https://accounts.spotify.com/authorize?client_id=4e7f97ca8905491ebc4e89f1674658dd&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+	"https://accounts.spotify.com/authorize?client_id=4e7f97ca8905491ebc4e89f1674658dd&response_type=code&redirect_uri=https://spotimusicvideo.netlify.app&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export default function Login() {
 	return (
